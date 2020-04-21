af88d90d52abdf5715c45f5648defcaed862848d
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-21 16:13:45 +0300

    Complete test for 'nth_largest' function

--- tests/test_nth_largest.py
+++ tests/test_nth_largest.py
@@ -21,3 +21,27 @@ def test_properties(elements_with_index: Tuple[MutableSequence[Domain], int],
     assert sorted(elements,
                   reverse=True,
                   key=key)[index] == result
+
+
+@given(strategies.elements_lists, strategies.keys)
+def test_first(elements: MutableSequence[Domain],
+               key: Key) -> None:
+    result = nth_largest(elements, 0,
+                         key=key)
+
+    assert result == (max(elements)
+                      if key is None
+                      else max(elements,
+                               key=key))
+
+
+@given(strategies.elements_lists, strategies.keys)
+def test_last(elements: MutableSequence[Domain],
+              key: Key) -> None:
+    result = nth_largest(elements, len(elements) - 1,
+                         key=key)
+
+    assert result == (min(elements)
+                      if key is None
+                      else min(elements,
+                               key=key))
